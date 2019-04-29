3534fe9fba524fd73b92854e90eeb27664adfde8
Mateus Koehler <mateus@mateuskoehler.com>
2019-04-29 19:12:36 -0400

    updated create-react-app with express

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home from './';
+
+class App extends React.Component {
+  state = {};
+
+  handleSearchSubmit = () => {
+    //axios query nyt
+  };
+}
+
+export default App;
