5f2c77ac9816a814bf1f752fdeea9306dd07c641
Mateus Koehler <mateus@mateuskoehler.com>
2019-05-04 13:57:37 -0400

    axios post request recieves user parameters and passes it in to the NYT api query, sends the results back in promise

--- routes/api/nytQuery.js
+++ routes/api/nytQuery.js
@@ -3,16 +3,15 @@ const nytQuery = require('express').Router();
 const axios = require('axios');
 const NYT_ID = process.env.NYT_ID;
 
-nytQuery.get('/getArticles', (req, res) => {
+nytQuery.post('/searchArticles', async (req, res) => {
   axios
-    .get(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=venezuela&sort=newest&api-key=${NYT_ID}`)
-    .then(result => {
-      res.send(result.data.response.docs);
-      // console.log(result.data.response);
+    .get(`https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=${NYT_ID}`, {
+      params: {
+        q: req.body.searchTerm
+      }
     })
-    .catch(error => {
-      res.send(error);
-    });
+    .then(result => res.send(result.data.response.docs))
+    .catch(error => res.send(error));
 });
 
 module.exports = nytQuery;
