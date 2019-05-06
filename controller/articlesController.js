5faddc119e4e1503d208c55b188bd1153cb0b437
Mateus Koehler <mateus@mateuskoehler.com>
2019-05-06 15:07:33 -0400

    moved api calls out of the routing and into controllers

---
+++ controller/articlesController.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const axios = require('axios');
+const NYT_ID = process.env.NYT_ID;
+
+module.exports = {
+  articleSearch: async (req, res) => {
+    axios
+      .get(`https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=${NYT_ID}`, {
+        params: {
+          q: req.body.searchTerm
+        }
+      })
+      .then(result => {
+        const articleData = result.data.response.docs.map(({ _id, headline, web_url }) => {
+          //_id, headline.main, pub_date, web_url
+          return {
+            _id,
+            headline,
+            web_url
+          };
+        });
+        res.send(articleData);
+      })
+      .catch(error => res.send(error));
+  }
+};
