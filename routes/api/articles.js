cf5449a2639eb20e689c4d39fbe927ba614d76fa
Mateus Koehler <mateus@mateuskoehler.com>
2019-05-08 10:06:14 -0400

    added routes to the saveArticle controller

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const articlesController = require('../../controller/articlesController');
 
-router.route('/').post(articlesController.articleSearch);
+router.route('/').post(articlesController.saveArticle);
 
 module.exports = router;
--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const articleRoutes = require('./articles');
 const newYorkTimes = require('./newYorkTimes');
 
-// router.use('/articles', articleRoutes);
+router.use('/articles', articleRoutes);
 router.use('/searchArticles', newYorkTimes);
 
 module.exports = router;
