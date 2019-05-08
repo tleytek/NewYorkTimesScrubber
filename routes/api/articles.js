0618e2fb55f3462f08aab143a3ed24c77a10cb18
Mateus Koehler <mateus@mateuskoehler.com>
2019-05-08 12:19:24 -0400

    added DB get request to controller

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const articlesController = require('../../controller/articlesController');
 
-router.route('/').post(articlesController.saveArticle);
+router
+  .route('/')
+  .post(articlesController.saveArticle)
+  .get(articlesController.getArticles);
 
 module.exports = router;
