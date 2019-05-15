70cabab0f7f4b7ecac9aa9b69d28f1b29a87ffa6
Mateus Koehler <mateus@mateuskoehler.com>
2019-05-15 14:45:55 -0400

    socket.io-client exposes an io global

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import Home from './components/Home';
 import Saved from './components/Saved';
+import socketIOClient from 'socket.io-client';
 import { Jumbotron } from './components/Jumbotron';
 import { Container } from './components/Grid';
+
+export const socket = socketIOClient('http://localhost:3001/');
+
 const App = () => {
   return (
     <div>
